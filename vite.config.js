cee71edfd14f4f42b3c8f9904e75fa79dac170c0
LucasA75 <lucasacevedocastro@gmail.com>
2023-02-08 23:15:39 -0300

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+/*AQUI AGREGAR EL SIGUIENTE CODIGO*/
+base: "/LucasA75/ParallaxReactPrueba/" 
+
 })
